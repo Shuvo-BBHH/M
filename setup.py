b5e067d4ec219f6184cb5f4674022673cc2b0e14
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-01-10 19:53:58 +0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="Naim",
+version='0.5',
+description='this is a simple pip modul and test',
+long_description='THia is simpile modul crate by mahdi hasan',
+package=['Naim'],
+install_requires=[]
+     )
