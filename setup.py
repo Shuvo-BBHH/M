803f41135aa93fc0533b438bcf01bfe7b8b1be94
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-01-10 20:39:46 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
-setup(name="Naim",
+setup(name="flamenaim",
 version='0.1',
 description='this is a simple pip modul and test',
 long_description='THia is simpile modul crate by mahdi hasan',
-package=['Naim'],
+package=['flamenaim'],
 install_requires=[])
