bcb63f12fd0526ded9d85673f065d199a825ec2b
Mahdi Hasan Shuvo <98658558+Shuvo-BBHH@users.noreply.github.com>
2023-01-16 11:03:51 +0600

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-setup(name="flamenaim",
-version='0.1',
-description='this is a simple pip modul and test',
-long_description='THia is simpile modul crate by mahdi hasan',
-package=['flamenaim'],
-install_requires=[])
